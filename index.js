0de822b766106acedc0fc67a08b75ba2421d052c
田志会 Chris <chris.tian@biposervice.com>
2022-01-13 22:30:27 +0800

    feat: traverse

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
+const fs = require("fs");
+const path = require("path");
 const { reg, regSakiGroup } = require("./config");
+
+module.exports.traverse = function (dir, list) {
+  fs.readdirSync(dir).forEach((file) => {
+    let fullPath = path.join(dir, file);
+    if (fs.lstatSync(fullPath).isDirectory()) {
+      traverseDir(fullPath);
+    } else {
+      list.push(fullPath);
+    }
+  });
+};
 
 module.exports.match = function (str) {
   let list = [];
   let matched;
   while ((matched = reg.exec(str)) != null) {
-    const saki = matched[regSakiGroup]
+    const saki = matched[regSakiGroup];
     list.push(saki);
   }
   return list;
---
+++ test/traverse.spec.js
@@ -0,0 +1,10 @@
+const { traverse, match } = require("../index");
+
+describe("traverse", function () {
+  it("scan file", function () {
+    const dir = "./examples";
+    const actual = [];
+    traverse(dir, actual);
+    expect(actual).toEqual(["examples/index.js"]);
+  });
+});
