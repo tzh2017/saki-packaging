aa08385ad822e6752ac7616d0fd877aab3886aa1
田志会 Chris <chris.tian@biposervice.com>
2022-04-12 18:27:11 +0800

    chore

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # saki-packaging
+
+```shell
+# 切换 node12
+nvm use
+# 执行
+node main --unhandled-rejections=strict
+```
--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   dir: "/Users/chris/Works/daisy",
-  exts: [".js", ".ts", ".vue", ".json"],
+  exts: [".js", ".ts", ".vue", ".dart"],
   excludePaths: ["node_modules", "uni_modules", "/i18n/"],
   reg: new RegExp(/(['"]([0-9A-Z]{6})['"])/, "g"),
   group: 2,
